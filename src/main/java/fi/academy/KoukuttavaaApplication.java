8f1f42c6d4b7478e8d639e8dc8f0471e70c61f99
svakel <sari.vakela@gmail.com>
2018-10-31 13:09:21 +0200

    sivusto luotu

---
+++ src/main/java/fi/academy/KoukuttavaaApplication.java
@@ -0,0 +1,12 @@
+package fi.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoukuttavaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoukuttavaaApplication.class, args);
+    }
+}
